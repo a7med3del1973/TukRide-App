4e631c45608dee04b79da96f64b9da39bf52c630
Ahmed Adel <a7med3del1973@gmail.com>
2024-07-07 09:18:07 +0300

    added userControllers funcations

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 // app.use(express.json());
-app.use("/api/test", (req, res) => {
+app.use('/api/test', (req, res) => {
   res.send(`Hello from the Tukdrive app...`);
 });
+
+// Routes
+app.use('/api/drivers', driverRouter);
+app.use('/api/users', userRouter);
+
+app.use('*', (req, res) => {
+  res.status(404).json({
+    status: 'fial',
+    message: `can't find ${req.originalUrl} on this server !`,
+  });
+});
+
 const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`TukRide is running on port ${PORT}...`);
