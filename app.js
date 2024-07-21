afa06eaa0747b2a3722684c7b7393cf64e112e70
Ahmed Adel <a7med3del1973@gmail.com>
2024-07-21 22:13:31 +0300

    added global error handler

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const app = express();
-
+const morgan = require('morgan');
 const userRoute = require('./routes/userRoutes');
 const driverRoute = require('./routes/driverRoutes');
-// const driverRoute = require('./routes/driverRoutes');
-
+const AppError = require('./utils/appError');
+const glopalErrorHandler = require('./controllers/errorController');
+const app = express();
 app.use(express.json());
 
 // app.use('/', (req, res) => {
@@ -13,6 +13,12 @@ app.use(express.json());
 // ROUTES
 app.use('/user', userRoute);
 app.use('/driver', driverRoute);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server !`));
+});
+app.use(glopalErrorHandler);
+
 const PORT = 8080;
 app.listen(PORT, (req, res) => {
   console.log(`Server is running on port ${PORT}..`);
---
+++ models/rideModel.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const rideSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    driver: {
+      type: Schema.Types.ObjectId,
+      ref: 'Driver',
+      required: true,
+    },
+    startLocation: {
+      type: {
+        type: String,
+        enum: ['Point'],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
+    },
+    endLocation: {
+      type: {
+        type: String,
+        enum: ['Point'],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
+    },
+    fare: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['pending', 'completed', 'cancelled'],
+      default: 'pending',
+    },
+    startTime: {
+      type: Date,
+      required: true,
+    },
+    endTime: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+
+const rideModel = mongoose.model('Ride', rideSchema);
+module.exports = rideModel;
